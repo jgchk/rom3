d663bcdcfe96f5c6c16781cf50d594cc61f1d62f
Jake Cheek <jake@f-m.fm>
2022-03-07 22:09:39 -0500

    prevent empty location input

--- src/components/LocationInput.tsx
+++ src/components/LocationInput.tsx
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled'
-import { FC } from 'react'
+import { FC, useMemo } from 'react'
 
 import {
   LocationInput as LocationInputType,
@@ -9,69 +9,72 @@ import {
 const LocationInput: FC<{
   value: LocationInputType[]
   onChange: (data: LocationInputType[]) => void
-}> = ({ value, onChange }) => (
-  <div>
-    {value.map((location, i) => (
-      <Container key={i}>
-        <input
-          placeholder='City'
-          value={location.city}
-          onChange={(e) =>
-            onChange(
-              value.map((loc, j) =>
-                j === i ? { ...loc, city: e.target.value } : loc
+}> = ({ value: rawValue, onChange }) => {
+  const value = useMemo(
+    () => (rawValue.length === 0 ? [makeLocation()] : rawValue),
+    [rawValue]
+  )
+
+  return (
+    <div>
+      {value.map((location, i) => (
+        <Container key={i}>
+          <input
+            placeholder='City'
+            value={location.city}
+            onChange={(e) =>
+              onChange(
+                value.map((loc, j) =>
+                  j === i ? { ...loc, city: e.target.value } : loc
+                )
               )
-            )
-          }
-        />
-        <input
-          placeholder='Region'
-          value={location.region}
-          onChange={(e) =>
-            onChange(
-              value.map((loc, j) =>
-                j === i ? { ...loc, region: e.target.value } : loc
+            }
+          />
+          <input
+            placeholder='Region'
+            value={location.region}
+            onChange={(e) =>
+              onChange(
+                value.map((loc, j) =>
+                  j === i ? { ...loc, region: e.target.value } : loc
+                )
               )
-            )
-          }
-        />
-        <input
-          placeholder='Country'
-          value={location.country}
-          onChange={(e) =>
-            onChange(
-              value.map((loc, j) =>
-                j === i ? { ...loc, country: e.target.value } : loc
+            }
+          />
+          <input
+            placeholder='Country'
+            value={location.country}
+            onChange={(e) =>
+              onChange(
+                value.map((loc, j) =>
+                  j === i ? { ...loc, country: e.target.value } : loc
+                )
               )
-            )
-          }
-        />
-        <button
-          type='button'
-          onClick={() => {
-            let val = value.filter((_, j) => j !== i)
-            if (val.length === 0) val = [makeLocation()]
-            onChange(val)
-          }}
-        >
-          -
-        </button>
-        <button
-          type='button'
-          onClick={() => {
-            onChange([
-              ...value.slice(0, i + 1),
-              makeLocation(),
-              ...value.slice(i + 1),
-            ])
-          }}
-        >
-          +
-        </button>
-      </Container>
-    ))}
-  </div>
-)
+            }
+          />
+          <button
+            type='button'
+            onClick={() => onChange(value.filter((_, j) => j !== i))}
+          >
+            -
+          </button>
+          <button
+            type='button'
+            onClick={() => {
+              onChange([
+                ...value.slice(0, i + 1),
+                makeLocation(),
+                ...value.slice(i + 1),
+              ])
+            }}
+          >
+            +
+          </button>
+        </Container>
+      ))}
+    </div>
+  )
+}
 
 export default LocationInput
 
