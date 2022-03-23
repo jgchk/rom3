14e8756b74b0b7b0b0f28556683185db23aa3fcb
Jake Cheek <jake@f-m.fm>
2022-03-22 23:21:26 -0400

    fix correction tree preview not showing children of deleted genres

--- src/modules/correction/hooks/useCorrectionGenresQuery.tsx
+++ src/modules/correction/hooks/useCorrectionGenresQuery.tsx
@@ -32,19 +32,26 @@ const useCorrectionGenresQuery = (
 
     const genres = [
       ...genresQuery.data
+        // remove deleted genres
         .filter((genre) => !deletedIds.has(genre.id))
         // replace edited genres with their real genres
         .map((genre) => editedIds[genre.id] ?? genre)
-        // replace edited parents/influences with their updated Ids
+        // remove deleted parents/influences & replace ids of parents/influences that have pending edits with the edited genre ids
         .map((genre) => ({
           ...genre,
-          parents: genre.parents.map(
-            (parentId) => editedIds[parentId]?.id ?? parentId
-          ),
-          influencedBy: genre.influencedBy.map((inf) => ({
-            ...inf,
-            id: editedIds[inf.id]?.id ?? inf.id,
-          })),
+          parents: genre.parents
+            // remove deleted genres
+            .filter((parentId) => !deletedIds.has(parentId))
+            // replace ids of genres that have pending edits with the edited genre id
+            .map((parentId) => editedIds[parentId]?.id ?? parentId),
+          influencedBy: genre.influencedBy
+            // remove deleted genres
+            .filter((inf) => !deletedIds.has(inf.id))
+            // replace ids of genres that have pending edits with the edited genre id
+            .map((inf) => ({
+              ...inf,
+              id: editedIds[inf.id]?.id ?? inf.id,
+            })),
         })),
       ...Object.values(correctionQuery.data.create),
     ]
