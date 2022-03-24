582ea386fd36d4d8223d19458a0da4db0c953e13
Jake Cheek <jake@f-m.fm>
2022-03-24 19:36:10 -0400

    style dialog with tailwind

--- src/common/components/Dialog.tsx
+++ src/common/components/Dialog.tsx
@@ -1,27 +1,12 @@
-import styled from '@emotion/styled'
 import { FC } from 'react'
 import { createPortal } from 'react-dom'
 
 const Dialog: FC = ({ children }) =>
   createPortal(
-    <Mask>
-      <DialogContainer>{children}</DialogContainer>
-    </Mask>,
+    <div className='absolute top-0 left-0 w-screen h-screen flex items-center justify-center bg-black bg-opacity-25 z-20'>
+      <div className='bg-white'>{children}</div>
+    </div>,
     document.body
   )
 
 export default Dialog
-
-const Mask = styled.div`
-  position: absolute;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 100vw;
-  height: 100vh;
-  background: rgb(0 0 0 / 25%);
-`
-
-const DialogContainer = styled.div`
-  background: white;
-`
