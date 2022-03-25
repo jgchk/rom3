cd706c49454f953ed462063aa839791a22bd942a
Jake Cheek <jake@f-m.fm>
2022-03-25 15:06:51 -0400

    unify genre form

--- src/modules/correction/components/forms/GenreForm.tsx
+++ src/modules/correction/components/forms/GenreForm.tsx
@@ -1,30 +1,19 @@
-import { Dispatch, FC, SetStateAction, useCallback } from 'react'
+import { Dispatch, FC, SetStateAction } from 'react'
 
+import { genreInfluencedByTypes } from '../../../../common/model/influences'
+import { genreParentTypes } from '../../../../common/model/parents'
 import { GenreApiInput } from '../../../server/routers/genres'
 import GenreTypeSelect from './elements/GenreTypeSelect'
-import MetaForm from './MetaForm'
-import SceneForm from './SceneForm'
-import StyleForm from './StyleForm'
-import TrendForm from './TrendForm'
+import InfluenceMultiselect from './elements/InfluenceMultiselect'
+import LocationInput from './elements/LocationInput'
+import MarkdownEditor from './elements/MarkdownEditor'
+import ParentMultiselect from './elements/ParentMultiselect'
+import StringArrayEditor from './elements/StringArrayEditor'
 
 const GenreForm: FC<{
   data: GenreApiInput
   onChange: Dispatch<SetStateAction<GenreApiInput>>
 }> = ({ data, onChange }) => {
-  // TODO: unify forms
-  const renderForm = useCallback(() => {
-    switch (data.type) {
-      case 'META':
-        return <MetaForm data={data} onChange={onChange} />
-      case 'SCENE':
-        return <SceneForm data={data} onChange={onChange} />
-      case 'STYLE':
-        return <StyleForm data={data} onChange={onChange} />
-      case 'TREND':
-        return <TrendForm data={data} onChange={onChange} />
-    }
-  }, [data, onChange])
-
   return (
     <div className='space-y-2'>
       <div>
@@ -46,7 +35,88 @@ const GenreForm: FC<{
           }}
         />
       </div>
-      {renderForm()}
+      <div>
+        <label className='block'>Name *</label>
+        <input
+          className='border border-gray-300'
+          value={data.name}
+          onChange={(e) => onChange((d) => ({ ...d, name: e.target.value }))}
+          required
+        />
+      </div>
+      <div>
+        <label className='block'>Alternate Names</label>
+        <label className='block text-sm text-gray-800'>
+          comma-separated list
+        </label>
+        <StringArrayEditor
+          value={data.alternateNames}
+          onChange={(alternateNames) =>
+            onChange((d) => ({ ...d, alternateNames }))
+          }
+        />
+      </div>
+      {genreParentTypes[data.type].length > 0 && (
+        <div>
+          <label className='block'>Parents</label>
+          <ParentMultiselect
+            parents={data.parents}
+            onChange={(parents) => onChange((d) => ({ ...d, parents }))}
+            childType={data.type}
+          />
+        </div>
+      )}
+      {genreInfluencedByTypes[data.type].length > 0 && (
+        <div>
+          <label className='block'>Influences</label>
+          <InfluenceMultiselect
+            influences={data.influencedBy}
+            onChange={(influencedBy) =>
+              onChange((d) => ({ ...d, influencedBy }))
+            }
+            childType={data.type}
+          />
+        </div>
+      )}
+      {data.type !== 'META' && (
+        <>
+          <div>
+            <label className='block'>Locations</label>
+            <LocationInput
+              value={data.locations}
+              onChange={(locations) => onChange((d) => ({ ...d, locations }))}
+            />
+          </div>
+          <div>
+            <label className='block'>Cultures</label>
+            <label className='block text-sm text-gray-800'>
+              comma-separated list
+            </label>
+            <StringArrayEditor
+              value={data.cultures}
+              onChange={(cultures) => onChange((d) => ({ ...d, cultures }))}
+            />
+          </div>
+        </>
+      )}
+      <div>
+        <label className='block'>Short Description *</label>
+        <textarea
+          className='border border-gray-300 w-full'
+          value={data.shortDesc}
+          onChange={(e) =>
+            onChange((d) => ({ ...d, shortDesc: e.target.value }))
+          }
+          required
+        />
+      </div>
+      <div>
+        <label className='block'>Long Description *</label>
+        <MarkdownEditor
+          value={data.longDesc}
+          onChange={(longDesc) => onChange((d) => ({ ...d, longDesc }))}
+        />
+      </div>
     </div>
   )
 }
