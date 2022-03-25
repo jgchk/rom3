cfc0cf91f77f8f4efb90c88a5793d53e9128b103
Jake Cheek <jake@f-m.fm>
2022-03-25 14:29:02 -0400

    trends can have trend children

--- src/common/model/parents.ts
+++ src/common/model/parents.ts
@@ -4,12 +4,12 @@ export const genreParentTypes: Record<GenreType, GenreType[]> = {
   META: ['META'],
   SCENE: [],
   STYLE: ['META', 'STYLE'],
-  TREND: ['META', 'STYLE'],
+  TREND: ['META', 'STYLE', 'TREND'],
 }
 
 export const genreChildTypes: Record<GenreType, GenreType[]> = {
   META: ['META', 'STYLE', 'TREND'],
   SCENE: [],
   STYLE: ['STYLE', 'TREND'],
-  TREND: [],
+  TREND: ['TREND'],
 }
