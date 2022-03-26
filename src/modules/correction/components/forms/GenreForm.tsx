712939fce3cbace3dca915815c5f6f496ded6c62
Jake Cheek <jake@f-m.fm>
2022-03-25 21:28:56 -0400

    label styling

--- src/modules/correction/components/forms/GenreForm.tsx
+++ src/modules/correction/components/forms/GenreForm.tsx
@@ -10,6 +10,10 @@ import MarkdownEditor from './elements/MarkdownEditor'
 import ParentMultiselect from './elements/ParentMultiselect'
 import StringArrayEditor from './elements/StringArrayEditor'
 
+const labelClassname = 'block text-sm font-medium text-gray-700'
+
+const smallLabelClassname = 'block text-xs font-medium text-gray-500'
+
 const GenreForm: FC<{
   data: GenreApiInput
   onChange: Dispatch<SetStateAction<GenreApiInput>>
@@ -18,14 +22,14 @@ const GenreForm: FC<{
   return (
     <div className='space-y-2'>
       <div>
-        <label className='block'>Type</label>
+        <label className={labelClassname}>Type</label>
         <GenreTypeSelect
           value={data.type}
           onChange={(type) => onChange((d) => ({ ...d, type }))}
         />
       </div>
       <div>
-        <label className='block'>Name *</label>
+        <label className={labelClassname}>Name *</label>
         <input
           className='border border-gray-300 px-2 py-1'
           value={data.name}
@@ -35,10 +39,8 @@ const GenreForm: FC<{
         />
       </div>
       <div>
-        <label className='block'>Alternate Names</label>
-        <label className='block text-sm text-gray-800'>
-          comma-separated list
-        </label>
+        <label className={labelClassname}>Alternate Names</label>
+        <label className={smallLabelClassname}>comma-separated list</label>
         <StringArrayEditor
           value={data.alternateNames}
           onChange={(alternateNames) =>
@@ -48,7 +50,7 @@ const GenreForm: FC<{
       </div>
       {genreParentTypes[data.type].length > 0 && (
         <div>
-          <label className='block'>Parents</label>
+          <label className={labelClassname}>Parents</label>
           <ParentMultiselect
             parents={data.parents}
             onChange={(parents) => onChange((d) => ({ ...d, parents }))}
@@ -59,7 +61,7 @@ const GenreForm: FC<{
       )}
       {genreInfluencedByTypes[data.type].length > 0 && (
         <div>
-          <label className='block'>Influences</label>
+          <label className={labelClassname}>Influences</label>
           <InfluenceMultiselect
             influences={data.influencedBy}
             onChange={(influencedBy) =>
@@ -73,17 +75,15 @@ const GenreForm: FC<{
       {data.type !== 'META' && (
         <>
           <div>
-            <label className='block'>Locations</label>
+            <label className={labelClassname}>Locations</label>
             <LocationInput
               value={data.locations}
               onChange={(locations) => onChange((d) => ({ ...d, locations }))}
             />
           </div>
           <div>
-            <label className='block'>Cultures</label>
-            <label className='block text-sm text-gray-800'>
-              comma-separated list
-            </label>
+            <label className={labelClassname}>Cultures</label>
+            <label className={smallLabelClassname}>comma-separated list</label>
             <StringArrayEditor
               value={data.cultures}
               onChange={(cultures) => onChange((d) => ({ ...d, cultures }))}
@@ -92,7 +92,7 @@ const GenreForm: FC<{
         </>
       )}
       <div>
-        <label className='block'>Short Description *</label>
+        <label className={labelClassname}>Short Description *</label>
         <textarea
           className='border border-gray-300 w-full px-2 py-1'
           value={data.shortDesc}
@@ -103,7 +103,7 @@ const GenreForm: FC<{
         />
       </div>
       <div>
-        <label className='block'>Long Description *</label>
+        <label className={labelClassname}>Long Description *</label>
         <MarkdownEditor
           value={data.longDesc}
           onChange={(longDesc) => onChange((d) => ({ ...d, longDesc }))}
