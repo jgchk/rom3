d092a15bd34f8c179f3b70d98afa2f18c84f179d
Jake Cheek <jake@f-m.fm>
2022-03-28 22:32:05 -0400

    redirect home page to genre tree

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,92 +1,13 @@
 import { NextPage } from 'next'
-import { FC, useCallback, useState } from 'react'
-import toast from 'react-hot-toast'
-
-import { useLoginMutation, useRegisterMutation } from '../common/services/auth'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 const Home: NextPage = () => {
-  return (
-    <div>
-      <Register />
-      <Login />
-    </div>
-  )
+  const { push: navigate } = useRouter()
+
+  useEffect(() => void navigate('/genres/tree'), [navigate])
+
+  return <div />
 }
 
 export default Home
-
-const Register: FC = () => {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-
-  const { mutate, isLoading } = useRegisterMutation()
-  const handleRegister = useCallback(
-    () =>
-      mutate(
-        { username, password },
-        {
-          onSuccess: (res) => {
-            toast.success(`Created account ${res.account.username}`)
-          },
-          onError: (error) => {
-            toast.error(error.message)
-          },
-        }
-      ),
-    [mutate, password, username]
-  )
-
-  return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault()
-      }}
-    >
-      <input value={username} onChange={(e) => setUsername(e.target.value)} />
-      <input value={password} onChange={(e) => setPassword(e.target.value)} />
-      <button
-        type='submit'
-        onClick={() => handleRegister()}
-        disabled={isLoading}
-      >
-        Register
-      </button>
-    </form>
-  )
-}
-
-const Login: FC = () => {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-
-  const { mutate, isLoading } = useLoginMutation()
-  const handleLogin = useCallback(
-    () =>
-      mutate(
-        { username, password },
-        {
-          onSuccess: (res) => {
-            toast.success(`Logged in ${res.account.username}`)
-          },
-          onError: (error) => {
-            toast.error(error.message)
-          },
-        }
-      ),
-    [mutate, password, username]
-  )
-
-  return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault()
-      }}
-    >
-      <input value={username} onChange={(e) => setUsername(e.target.value)} />
-      <input value={password} onChange={(e) => setPassword(e.target.value)} />
-      <button type='submit' onClick={() => handleLogin()} disabled={isLoading}>
-        Login
-      </button>
-    </form>
-  )
-}
