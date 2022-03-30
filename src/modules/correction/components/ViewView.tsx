3c341dcbe643010f2fe4d37cca48dddde25c31ca
Jake Cheek <jake@f-m.fm>
2022-03-30 18:19:01 -0400

    make correction genre view page look like normal view page

--- src/modules/correction/components/ViewView.tsx
+++ src/modules/correction/components/ViewView.tsx
@@ -1,15 +1,14 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import { FC, useCallback, useState } from 'react'
+import { FC, useState } from 'react'
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi'
 
+import ButtonSecondary from '../../../common/components/ButtonSecondary'
 import useGenreTypeColor from '../../../common/hooks/useGenreTypeColor'
 import { GenreApiOutput } from '../../../common/model'
-import { ApiGenreInfluence } from '../../../common/services/genres'
 import { useCorrectionContext } from '../contexts/CorrectionContext'
 import useCorrectionGenreQuery from '../hooks/useCorrectionGenreQuery'
 import useIsMyCorrectionQuery from '../hooks/useIsMyCorrectionQuery'
-import ParentsView from './ParentsView'
-import TreeView from './TreeView'
 
 const ViewView: FC<{ genreId: number }> = ({ genreId }) => {
   const { id: correctionId } = useCorrectionContext()
@@ -26,117 +25,71 @@ const ViewView: FC<{ genreId: number }> = ({ genreId }) => {
 const Loaded: FC<{
   genre: GenreApiOutput
 }> = ({ genre }) => {
+  const [expanded, setExpanded] = useState(false)
+
   const { id: correctionId } = useCorrectionContext()
   const { data: isMyCorrection } = useIsMyCorrectionQuery(correctionId)
 
-  const { asPath } = useRouter()
-
-  const [expanded, setExpanded] = useState(false)
-
   const color = useGenreTypeColor(genre.type)
 
-  const renderDescription = useCallback(() => {
-    if (genre.shortDesc && genre.longDesc) {
-      return (
-        <div className='mt-3'>
-          {/* TODO: render markdown for long desc */}
-          <div>{expanded ? genre.longDesc : genre.shortDesc}</div>
-          <button
-            className='text-sm font-semibold text-stone-700 hover:text-primary-600'
-            onClick={() => setExpanded(!expanded)}
-          >
-            {expanded ? 'Show Short Description' : 'Show Long Description'}
-          </button>
-        </div>
-      )
-    }
-
-    if (genre.shortDesc) {
-      return <div className='mt-3'>{genre.shortDesc}</div>
-    }
-
-    if (genre.longDesc) {
-      return (
-        <div className='mt-3'>
-          {/* TODO: render markdown for long desc */}
-          {expanded && <div>{genre.longDesc}</div>}
-          <button
-            className='text-sm font-semibold text-stone-700 hover:text-primary-600'
-            onClick={() => setExpanded(!expanded)}
-          >
-            {expanded ? 'Show Short Description' : 'Show Long Description'}
-          </button>
-        </div>
-      )
-    }
-
-    return null
-  }, [expanded, genre.longDesc, genre.shortDesc])
+  const { asPath } = useRouter()
 
   return (
-    <div>
-      {isMyCorrection && (
-        <Link
-          href={{
-            pathname: `/corrections/${correctionId}/genres/edit`,
-            query: { genreId: genre.id, from: asPath },
-          }}
-        >
-          <a>Edit</a>
-        </Link>
-      )}
+    <div className='space-y-4'>
+      <Breadcrumbs genre={genre} />
 
-      <div className='text-xs font-bold'>
-        <span className={color}>{genre.type}</span>
-        {genre.trial && (
-          <>
-            {' '}
-            <span className='text-stone-500'>(TRIAL)</span>
-          </>
-        )}
-      </div>
-      <div className='text-xl font-medium mt-0.5'>{genre.name}</div>
-      {genre.alternateNames.length > 0 && (
-        <div className='text-stone-600 font-medium text-sm mt-1'>
-          AKA: {genre.alternateNames.join(', ')}
-        </div>
-      )}
-      {genre.locations.length > 0 && (
-        <div className='text-stone-600 font-medium text-sm mt-1'>
-          Locations:{' '}
-          {genre.locations.map((loc) =>
-            [loc.city, loc.country, loc.region]
-              .filter((s) => s.length > 0)
-              .join(', ')
+      <div className='bg-white border border-stone-300 shadow-sm p-6'>
+        <div className='text-xs font-bold'>
+          <span className={color}>{genre.type}</span>
+          {genre.trial && (
+            <>
+              {' '}
+              <span className='text-stone-500'>(TRIAL)</span>
+            </>
           )}
         </div>
-      )}
-      {genre.cultures.length > 0 && (
-        <div className='text-stone-600 font-medium text-sm mt-1'>
-          Cultures: {genre.cultures.join(', ')}
-        </div>
-      )}
-      {renderDescription()}
-      {genre.parents.length > 0 && (
-        <div className='space-y-3 bg-stone-100 px-4 py-3 mt-6'>
-          <div className='text-stone-600 font-medium'>Parents</div>
-          <ParentsView childId={genre.id} />
-        </div>
-      )}
-      <div className='space-y-3 bg-stone-100 px-4 py-3 mt-6'>
-        <div className='text-stone-600 font-medium'>Children</div>
-        <TreeView parentId={genre.id} />
+        <div className='text-xl font-medium mt-0.5'>{genre.name}</div>
+        {genre.alternateNames.length > 0 && (
+          <div className='text-stone-600 font-medium text-sm mt-1'>
+            AKA: {genre.alternateNames.join(', ')}
+          </div>
+        )}
+        {genre.locations.length > 0 && (
+          <div className='text-stone-600 font-medium text-sm mt-1'>
+            Locations:{' '}
+            {genre.locations.map((loc) =>
+              [loc.city, loc.country, loc.region]
+                .filter((s) => s.length > 0)
+                .join(', ')
+            )}
+          </div>
+        )}
+        {genre.cultures.length > 0 && (
+          <div className='text-stone-600 font-medium text-sm mt-1'>
+            Cultures: {genre.cultures.join(', ')}
+          </div>
+        )}
+        <Description
+          genre={genre}
+          expanded={expanded}
+          onExpandChange={(expanded) => setExpanded(expanded)}
+        />
       </div>
-      {genre.influencedBy.length > 0 && (
-        <div className='space-y-3 bg-stone-100 px-4 py-3 mt-6'>
-          <div className='text-stone-600 font-medium'>Influences</div>
-          <ul className='space-y-3'>
-            {genre.influencedBy.map((inf) => (
-              <li key={inf.id}>
-                <Influence {...inf} />
-              </li>
-            ))}
-          </ul>
+
+      <Hierarchy genre={genre} />
+
+      {isMyCorrection && (
+        <div>
+          <Link
+            href={{
+              pathname: `/corrections/${correctionId}/genres/edit`,
+              query: { genreId: genre.id, from: asPath },
+            }}
+          >
+            <a>
+              <ButtonSecondary>Edit</ButtonSecondary>
+            </a>
+          </Link>
         </div>
       )}
     </div>
@@ -145,11 +98,120 @@ const Loaded: FC<{
 
 export default ViewView
 
-const Influence: FC<ApiGenreInfluence> = ({ id, influenceType }) => {
+const Description: FC<{
+  genre: GenreApiOutput
+  expanded: boolean
+  onExpandChange: (expanded: boolean) => void
+}> = ({ genre, expanded, onExpandChange }) => {
+  if (genre.shortDesc && genre.longDesc) {
+    return (
+      <div className='mt-3'>
+        {/* TODO: render markdown for long desc */}
+        <div>{expanded ? genre.longDesc : genre.shortDesc}</div>
+        <button
+          className='text-sm font-semibold text-stone-700 hover:text-primary-600'
+          onClick={() => onExpandChange(!expanded)}
+        >
+          {expanded ? 'Show Short Description' : 'Show Long Description'}
+        </button>
+      </div>
+    )
+  }
+
+  if (genre.shortDesc) {
+    return <div className='mt-3'>{genre.shortDesc}</div>
+  }
+
+  if (genre.longDesc) {
+    return (
+      <div className='mt-3'>
+        {/* TODO: render markdown for long desc */}
+        {expanded && <div>{genre.longDesc}</div>}
+        <button
+          className='text-sm font-semibold text-stone-700 hover:text-primary-600'
+          onClick={() => onExpandChange(!expanded)}
+        >
+          {expanded ? 'Show Short Description' : 'Show Long Description'}
+        </button>
+      </div>
+    )
+  }
+
+  return null
+}
+
+const Breadcrumbs: FC<{ genre: GenreApiOutput }> = ({ genre }) => {
+  const { id: correctionId } = useCorrectionContext()
+
+  return (
+    <ol className='flex items-center text-sm text-stone-500'>
+      <li>
+        <Link href={`/corrections/${correctionId}/tree`}>
+          <a className='font-medium group flex items-center px-2 py-1'>
+            <HiChevronLeft className='mr-1' />
+            <span className='group-hover:underline'>All Genres</span>
+          </a>
+        </Link>
+      </li>
+      {genre.parents.length > 0 && <HiChevronRight />}
+      {genre.parents.map((id, i) => (
+        <li key={id}>
+          {i !== 0 && '|'}
+          <Breadcrumb id={id} />
+        </li>
+      ))}
+      <HiChevronRight />
+      <div className='font-semibold px-2 py-1'>{genre.name}</div>
+    </ol>
+  )
+}
+
+const Breadcrumb: FC<{ id: number }> = ({ id }) => {
   const { id: correctionId } = useCorrectionContext()
   const { data } = useCorrectionGenreQuery(id, correctionId)
 
-  if (!data) return <div>Loading...</div>
+  return (
+    <Link
+      href={{
+        pathname: `/corrections/${correctionId}/genres/view`,
+        query: { genreId: id },
+      }}
+    >
+      <a className='text-sm text-stone-500 font-medium inline-flex items-center px-2 py-1 hover:underline'>
+        {data ? data.name : 'Loading...'}
+      </a>
+    </Link>
+  )
+}
+
+const Hierarchy: FC<{ genre: GenreApiOutput }> = ({ genre }) => (
+  <div className='bg-white shadow-sm border border-stone-300'>
+    <div className='px-2 py-1 text-stone-600 font-medium border-b border-stone-200'>
+      Hierarchy
+    </div>
+    <ul className='px-6 pt-5 pb-3 space-y-4'>
+      {genre.parents.map((id) => (
+        <li key={id}>
+          <Parent id={id} />
+        </li>
+      ))}
+
+      <li>
+        <div className='text-stone-600 font-medium text-lg'>{genre.name}</div>
+
+        <Children childIds={genre.children} />
+      </li>
+    </ul>
+  </div>
+)
+
+const Parent: FC<{ id: number }> = ({ id }) => {
+  const { id: correctionId } = useCorrectionContext()
+  const { data } = useCorrectionGenreQuery(id, correctionId)
+
+  if (!data) {
+    return <div>Loading...</div>
+  }
 
   return (
     <div>
@@ -159,20 +221,52 @@ const Influence: FC<ApiGenreInfluence> = ({ id, influenceType }) => {
           query: { genreId: id },
         }}
       >
-        <a className='font-semibold text-primary-600 hover:underline'>
+        <a className='text-primary-600 font-semibold hover:underline'>
           {data.name}
         </a>
       </Link>
-      {influenceType && (
-        <div className='text-sm font-semibold text-stone-500'>
-          {influenceType}
-        </div>
-      )}
-      {data.shortDesc && (
-        <div className='text-sm font-medium text-stone-700'>
-          {data.shortDesc}
-        </div>
-      )}
+
+      <div className='text-sm text-stone-600'>{data.shortDesc}</div>
     </div>
   )
 }
+
+const Child: FC<{ id: number }> = ({ id }) => {
+  const { id: correctionId } = useCorrectionContext()
+  const { data } = useCorrectionGenreQuery(id, correctionId)
+
+  if (!data) {
+    return <div>Loading...</div>
+  }
+
+  return (
+    <div>
+      <div className='py-2'>
+        <Link
+          href={{
+            pathname: `/corrections/${correctionId}/genres/view`,
+            query: { genreId: id },
+          }}
+        >
+          <a className='text-primary-600 font-semibold hover:underline'>
+            {data.name}
+          </a>
+        </Link>
+
+        <div className='text-sm text-stone-600'>{data.shortDesc}</div>
+      </div>
+
+      <Children childIds={data.children} />
+    </div>
+  )
+}
+
+const Children: FC<{ childIds: number[] }> = ({ childIds }) => (
+  <ul className='mt-4 space-y-4'>
+    {childIds.map((id) => (
+      <li className='pl-6 border-l-2 border-primary-600' key={id}>
+        <Child id={id} />
+      </li>
+    ))}
+  </ul>
+)
