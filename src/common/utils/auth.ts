ee5ef2dfff8747d8639b60768f169f3f059f3509
Jake Cheek <jake@f-m.fm>
2022-03-31 22:24:30 -0400

    set cookie expiration to 30 days

--- src/common/utils/auth.ts
+++ src/common/utils/auth.ts
@@ -5,6 +5,6 @@ export const tokenKey = 'token'
 export const getToken = () => cookies.get(tokenKey)
 
 export const setToken = (token: string) =>
-  cookies.set(tokenKey, token, { expires: 365 })
+  cookies.set(tokenKey, token, { expires: 30 })
 
 export const clearToken = () => cookies.remove(tokenKey)
