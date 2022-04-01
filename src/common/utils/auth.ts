3c52e432c061ca09083aa27ac84036468519f6dc
Jake Cheek <jake@f-m.fm>
2022-03-31 22:23:50 -0400

    increase cookie storage length

--- src/common/utils/auth.ts
+++ src/common/utils/auth.ts
@@ -4,6 +4,7 @@ export const tokenKey = 'token'
 
 export const getToken = () => cookies.get(tokenKey)
 
-export const setToken = (token: string) => cookies.set(tokenKey, token)
+export const setToken = (token: string) =>
+  cookies.set(tokenKey, token, { expires: 365 })
 
 export const clearToken = () => cookies.remove(tokenKey)
